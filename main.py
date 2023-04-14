bd4e3b6ccc3cb170e6d53c7f20457a9f1da95bb7
lukasmisiukevicius <misiukeviciuslukas@yahoo.com>
2023-04-14 09:23:38 +0300

    made packages

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from shop.shop_items import Food
+from shop.shop_items import Drink
+from shop.shop_items import Item
+
+from shop.shop_customers import Customer
+
+my_food = Food()
+my_drink = Drink()
+my_item = Item()
+my_customer = Customer()
