9cbfdc32a32861cea7ae0434db9b8e5ae524fa1c
lukasmisiukevicius <misiukeviciuslukas@yahoo.com>
2023-04-15 12:53:31 +0300

    fourth done

--- main.py
+++ main.py
@@ -36,8 +36,18 @@ def thirdTask():
     print(d2.full_info())
 
 def fourthTask():
-    pass
+    c1 = Customer("Jonas Jonaitis", [Food("Batonas", 2, 1.3), Drink("CocaCola", 3, 1.7)])
+    c2 = Customer("Petras Petraitis", [Food("Sviestas", 1, 1.3), Drink("Sprite", 2, 1.7)])
 
+    print(c1.get_items())
+    print(c2.get_items())
+
+    c1.add_item(Drink("Fanta", 10, 1.7))
+    print(c1.get_items())
+
+    c2.remove_item(2)
+    c2.remove_item(1)
+    print(c2.get_items())
 
 if __name__ == '__main__':
     while(1):
